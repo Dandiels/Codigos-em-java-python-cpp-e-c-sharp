49be0bded70d66442e55e44954b849ccfee20f8d
Dandiels <101065507+Dandiels@users.noreply.github.com>
2022-10-25 10:38:08 -0300

    Delete Q6.py

--- Q6.py
+++
@@ -1,5 +0,0 @@
-anos=int(input("Digite a sua idade: "))
-meses=int(input("Digite quantos meses se passaram desde seu aniversário: "))
-dias=int(input("Digite quantos dias você viveu no mês atual: "))
-diasVividos=anos*365+meses*30+dias
-print(f"Você viveu um total de {diasVividos} dias.")
