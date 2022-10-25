b71e34ca86e6977c5fa1d261af78bcbef2127240
Dandiels <101065507+Dandiels@users.noreply.github.com>
2022-10-25 10:36:34 -0300

    Delete Q12.py

--- Q12.py
+++
@@ -1,6 +0,0 @@
-peso2=float(input("Digite a nota de peso 2: "))
-peso3=float(input("Digite a nota de peso 3: "))
-peso4=float(input("Digite a nota de peso 4: "))
-peso7=float(input("Digite a nota de peso 7: "))
-média_final=(peso2*2+peso3*3+peso4*4+peso7*7)/16
-print(f"A média final desse aluno é {média_final:.1f}.")
